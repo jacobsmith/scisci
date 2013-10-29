7e22bc21f777255118d1b176c40e4ff013886bb3
Jacob Smith <jacob.wesley.smith@gmail.com>
2013-10-28 20:39:11 -0400

    ~fixed inputs, need to refactor the code :"

--- form_helpers.rb
+++ form_helpers.rb
@@ -15,14 +15,17 @@ def create_inputs(type, labels, value = nil, columns = 0, rows = 0)
     saved_value = @request.respond_to?(label) ? @request.send(label) : "" 
     element << '<div class="control-group">'
     element <<  '<label class="control-label" for="' + label + '">' + underscore_to_words(label) + '</label>'
-    element << '<div class="controls">'
-    element <<  "<input type=\"" + type + "\""
-    element << "\" style=\"height:100px; width:300px;\"" if type == 'textarea' 
+    element << '<div class="controls span0">'
+    element <<  "<input type=\"" + type + "\" class=\"span4\"" if type != 'textarea'
+    element << '<textarea class="span4" rows="4" ' if type == 'textarea'
     element << " id=\"" + label + "\" name=\"" + label + "\" value=\"" + saved_value + "\">"
+#    element << '</input>' if type != 'textarea'
+    element << '</textarea>' if type == 'textarea'
     element << '</div>'
     element << '</div>'
     element << "\n"
     element << "<br>" if label == labels[-1] 
+    element << "<br>"
   end
   return element 
 end
