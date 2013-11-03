2e12dec36da3458e21211f8e24e0889a4fb0086f
Jacob Smith <jacob.wesley.smith@gmail.com>
2013-11-03 12:14:51 -0500

    tags now all show up (rather than just first one)

--- views/view_helpers.rb
+++ views/view_helpers.rb
@@ -1,8 +1,10 @@
 def tag_links(note)
   if note.tags != ''
+    all_tags = ''
     note.tags.split(",").each do |tag| 
-      return link_to("#" + tag.strip, "/tag/#{tag}") 
+      all_tags += link_to(" #" + tag.strip, "/tag/#{tag}")
     end
+    return all_tags
   else
     return ''
   end 
