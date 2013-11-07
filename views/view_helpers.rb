5db76b167b772fffc0fa29bff9116f2f8f0f3715
Jacob Smith <jacob.wesley.smith@gmail.com>
2013-11-06 19:09:19 -0500

    should be able to add tags by clicking on them now (:

--- views/view_helpers.rb
+++ views/view_helpers.rb
@@ -10,14 +10,24 @@ def tag_links(note)
   end
 end
 
-def tag_append(note)
-  if note.tags != ''
-    all_tags = ''
-    note.tags.split(",").each do |tag|
-      all_tags += link_to(" #" + tag.strip, "#tag_to_append", "tags_to_append")
+def tag_append(source)
+    
+  @all_tags = {}
+  @all_tags.default = 0
+
+  source.note.map { |note|
+    if note.tags != ""
+      note.tags.split(",").each do |tag|
+        @all_tags[tag.strip] += 1
+      end
     end
-    return all_tags
-  else
-    return ''
-  end
+  } 
+  sorted_tags = @all_tags.sort_by { |key, value| value }.reverse
+
+  #doing a hack to return tags with links--not the best solution, but working
+  tags = "" 
+  sorted_tags.each { |tag, value| tags += link_to(" #" + tag.to_s, "#tag_to_append", "tags_to_append") }
+  return tags
 end
+  
+
